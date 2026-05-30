2adf759f278b623b31bfa6bac1e5eaeb3176ccae
discoveringchiayi <discoveringchiayi@gmail.com>
2026-05-31 03:45:38 +0800

    新增系館

---
+++ 資管系+管理學院B棟/app-files/data.js
@@ -0,0 +1,863 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1f-1b-4",
+      "name": "1F-1(接B-4)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.1042006253772954,
+        "pitch": -0.05870438554630475,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.007190769503117,
+          "pitch": 0.09630191286930412,
+          "rotation": 0,
+          "target": "1-1f-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.998450541829852,
+          "pitch": -0.16271982517574735,
+          "title": "管理學院B棟",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-1f-2",
+      "name": "1F-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.5434690543295773,
+        "pitch": 0.28600415214026853,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7236917090615576,
+          "pitch": 0.2389494004748638,
+          "rotation": 0,
+          "target": "0-1f-1b-4"
+        },
+        {
+          "yaw": -0.2793308426507384,
+          "pitch": 0.1775235879562871,
+          "rotation": 0,
+          "target": "2-1f-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-1f-3",
+      "name": "1F-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.9716760908458664,
+        "pitch": 0.22876954052551746,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9577630133740982,
+          "pitch": 0.23432569050130425,
+          "rotation": 0,
+          "target": "3-1f-4"
+        },
+        {
+          "yaw": 1.2374060872120367,
+          "pitch": 0.17125015475489036,
+          "rotation": 0,
+          "target": "1-1f-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-1f-4",
+      "name": "1F-4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.044206571212589,
+        "pitch": 0.07504020724289262,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.016064741472362698,
+          "pitch": 0.11837310554221148,
+          "rotation": 0,
+          "target": "2-1f-3"
+        },
+        {
+          "yaw": -2.8605597969078573,
+          "pitch": 0.18749250103491732,
+          "rotation": 5.497787143782138,
+          "target": "10-2f-2"
+        },
+        {
+          "yaw": 2.5995606911197022,
+          "pitch": 0.22937763034951963,
+          "rotation": 0.7853981633974483,
+          "target": "4-1f-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-1f-5",
+      "name": "1F-5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.06530838994976307,
+        "pitch": -0.08583123042256702,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3477474065712194,
+          "pitch": 0.1144701066836955,
+          "rotation": 0,
+          "target": "5-1f-6b-11b-9"
+        },
+        {
+          "yaw": 3.0564982360598876,
+          "pitch": 0.2805875541889371,
+          "rotation": 0,
+          "target": "3-1f-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-1f-6b-11b-9",
+      "name": "1F-6(前接B-11)(左接B-9))",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.6541644377624563,
+        "pitch": 0.35152614998674636,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0335371205697257,
+          "pitch": 0.21075634771974805,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": -1.624350264225491,
+          "pitch": 0.12547335144584792,
+          "rotation": 0,
+          "target": "4-1f-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-1f-7",
+      "name": "1F-7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5191961213921914,
+          "pitch": 0.03037839457334357,
+          "rotation": 0,
+          "target": "15-2f-8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-ab-18",
+      "name": "A棟穿堂(接B-18)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.5840774101274953,
+        "pitch": -0.14836223329150577,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6787163924153141,
+          "pitch": 0.10640611721168014,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "戶外",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.1610654558563844,
+        "pitch": 0.103341342830511,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.48132388871609066,
+          "pitch": 0.04752868125477505,
+          "rotation": 0,
+          "target": "5-1f-6b-11b-9"
+        },
+        {
+          "yaw": 2.817919286621173,
+          "pitch": 0.09013653815010159,
+          "rotation": 0,
+          "target": "7-ab-18"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-2f-1",
+      "name": "2F-1資訊管理學系管理資訊系統專案實驗室",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.5322953636925556,
+        "pitch": 0.3705589281412571,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3693064014175693,
+          "pitch": 0.1308749310365691,
+          "rotation": 0.7853981633974483,
+          "target": "10-2f-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-2f-2",
+      "name": "2F-2實驗室外走廊",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21620772024781054,
+          "pitch": 0.04651505884389806,
+          "rotation": 0,
+          "target": "9-2f-1"
+        },
+        {
+          "yaw": -2.83201684414648,
+          "pitch": 0.4150195119053137,
+          "rotation": 7.853981633974483,
+          "target": "3-1f-4"
+        },
+        {
+          "yaw": -1.7859004559551792,
+          "pitch": 0.21275954356899973,
+          "rotation": 0,
+          "target": "11-2f-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.21681039583227957,
+          "pitch": -0.13045716372876015,
+          "title": "資訊管理學系管理資訊系統專案實驗室",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "11-2f-3",
+      "name": "2F-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.0251411285116987,
+        "pitch": 0.21367024536835189,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.032388711502349565,
+          "pitch": 0.07541711990727151,
+          "rotation": 0,
+          "target": "13-2f-5"
+        },
+        {
+          "yaw": -3.0884821464269105,
+          "pitch": 0.21507335705655706,
+          "rotation": 0,
+          "target": "10-2f-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-2f-4",
+      "name": "2F-4元宇宙數位教室",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.8070137326482474,
+        "pitch": 0.21892727759798802,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.46145823032596844,
+          "pitch": -0.037985212407129154,
+          "rotation": 1.5707963267948966,
+          "target": "13-2f-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-2f-5",
+      "name": "2F-5元宇宙教室外走廊",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.0388067475401463,
+        "pitch": 0.08006556654378372,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2587886973930864,
+          "pitch": 0.22318222903255602,
+          "rotation": 0,
+          "target": "14-2f-6"
+        },
+        {
+          "yaw": -2.940704460396608,
+          "pitch": 0.07004050962009423,
+          "rotation": 12.566370614359176,
+          "target": "12-2f-4"
+        },
+        {
+          "yaw": -1.5761101789616792,
+          "pitch": 0.20960380660148026,
+          "rotation": 0,
+          "target": "11-2f-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.903350209986293,
+          "pitch": -0.24840348568088189,
+          "title": "元宇宙數位教室",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "14-2f-6",
+      "name": "2F-6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.673578829641908,
+        "pitch": 0.3433028246476386,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1921712871565013,
+          "pitch": 0.15686130622975725,
+          "rotation": 6.283185307179586,
+          "target": "15-2f-8"
+        },
+        {
+          "yaw": 2.5709902500601167,
+          "pitch": 0.34288380365496174,
+          "rotation": 0,
+          "target": "13-2f-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-2f-8",
+      "name": "2F-8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.6361898953592018,
+        "pitch": 0.22957345146624775,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19460566297674298,
+          "pitch": 0.0476465557924719,
+          "rotation": 3.141592653589793,
+          "target": "6-1f-7"
+        },
+        {
+          "yaw": 0.45238583285232536,
+          "pitch": 0.0654730833610131,
+          "rotation": 0,
+          "target": "17-4f-2"
+        },
+        {
+          "yaw": 1.143021307934836,
+          "pitch": 0.13834430599653658,
+          "rotation": 6.283185307179586,
+          "target": "14-2f-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-4f-1",
+      "name": "4F-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.182586539307792,
+        "pitch": 0.05820498777412908,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03228114076759425,
+          "pitch": 0.1917232798887536,
+          "rotation": 0,
+          "target": "17-4f-2"
+        },
+        {
+          "yaw": -2.182581627599834,
+          "pitch": -0.07863394221359243,
+          "rotation": 7.853981633974483,
+          "target": "18-4f-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.3730689420194153,
+          "pitch": -0.325662857135395,
+          "title": "電子商務研究中心",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "17-4f-2",
+      "name": "4F-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.7374875817241247,
+        "pitch": 0.14831158792866894,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6544089823871833,
+          "pitch": 0.19076149609468018,
+          "rotation": 2.356194490192345,
+          "target": "15-2f-8"
+        },
+        {
+          "yaw": -2.400832438904118,
+          "pitch": 0.23536966449778163,
+          "rotation": 0,
+          "target": "16-4f-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "18-4f-3",
+      "name": "4F-3電子商務研究中心",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.42470831154357747,
+        "pitch": 0.2235501519687979,
+        "fov": 1.4616456383387482
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08613572011406667,
+          "pitch": 0.07105897659942784,
+          "rotation": 0,
+          "target": "16-4f-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
